e73256b125eed11e2a82f77629ffd257071c3f7a
FACUU04 <facundososa550@gmail.com>
2024-12-12 16:41:27 -0300

    Project of the game with java

---
+++ Juego_NumeroSecreto.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+
+
+        int intentos = 0;
+        int numeroSecreto= new Random().nextInt(100);
+        System.out.println(numeroSecreto);
+
+        System.out.println("Juguemos a adivinar el numero secreto, escribe un numero entre 1 y 100" +
+                " (tienes 5 intentos)");
+        while (intentos < 5){
+            Scanner teclado = new Scanner(System.in);
+            int respuestaUser = teclado.nextInt();
+
+            if (respuestaUser == numeroSecreto){
+                System.out.println("FELCIDADESS GANASTEE. adivinazte el numero secreto en: "+intentos +" intentos");
+                break;
+
+        } else if (respuestaUser > numeroSecreto) {
+                System.out.println("Te fuistee largo, el numero secreto es mas chico");
+                 intentos++;
+            }  else {
+                System.out.println("ASHH estuviste cerca, prueba otra vez, (pista, el numerosecreto es mayor)");
+                intentos++;
+            }
+            while (intentos == 5){
+                System.out.println("GAME OVER! limite de intentos, el numero secreto era "+ numeroSecreto);
+                break;
+            }
+
+    }
+    }
+}
